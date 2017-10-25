f33c562ac6010a718c64205c10743b1bc066e6ee
hayabusa88magic <30853550+hayabusa88magic@users.noreply.github.com>
2017-10-25 18:06:32 +0900

    001

---
+++ app/src/main/java/com/example/ble_final/BleServer.java
@@ -0,0 +1,8 @@
+package com.example.ble_final;
+
+/**
+ * Created by Shunya_F on 2017/10/25.
+ */
+
+public class BleServer {
+}
--- app/src/main/java/com/example/ble_final/MainActivity.java
+++ app/src/main/java/com/example/ble_final/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.ble_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import java.util.UUID;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BLE_Connect";
+    BleCliant BleC = new BleCliant();
+    BleServer BleS = new BleServer();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        スタートボタンの探索
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+                BleC.connect();
+
+            }
+        });
+
     }
 }
